5bc371515b09e88ea8f051651755708e6072234c
msavdert <melihsavdert@gmail.com>
2023-09-28 11:14:47 -0400

    docs-only 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/msavdert/docusaurus-melih/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/msavdert/docusaurus-melih/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
