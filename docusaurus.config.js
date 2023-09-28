2d02c42f17b2fa32650742efcc4eb83319168bcd
msavdert <melihsavdert@gmail.com>
2023-09-28 00:32:45 -0400

    change all title and site name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Melih's Blog",
+  title: "Melih's Site",
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -64,9 +64,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: "Melih's Blog",
+        title: "Melih's Site",
         logo: {
-          alt: "Melih's Blog Logo",
+          alt: "Melih's Site Logo",
           src: 'img/logo.svg',
         },
         items: [
