35f80eed4ee5eef9a01f632d527271a8d5207611
msavdert <melihsavdert@gmail.com>
2023-09-28 18:25:56 -0400

    collapse sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,6 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          sidebarCollapsed: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
