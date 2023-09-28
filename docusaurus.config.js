bb2f950f9f7e6aab63151b671a102292a77b339f
msavdert <melihsavdert@gmail.com>
2023-09-28 00:31:03 -0400

    change site title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,9 +64,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: "Melih's Blog",
         logo: {
-          alt: 'My Site Logo',
+          alt: "Melih's Blog Logo",
           src: 'img/logo.svg',
         },
         items: [
